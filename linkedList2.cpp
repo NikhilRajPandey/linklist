5065536ba6d7adf43d7b48e6bd452760251d3460
NikhilRajPandey <nikhilrajpandey1@gmail.com>
2020-08-18 21:26:29 +0530

    Updated Some Function and added some more function

--- linkedList2.cpp
+++ linkedList2.cpp
@@ -9,21 +9,22 @@ class Node
 class LinkList
 {
     private:
-        int used_size = 0;
+        int used_size = 0; // For 1 Element it is 0
         Node *FirstElement = new Node;
         Node *LastElement = FirstElement;
         // By Default i am keeping this but when new element will come i will update this value
     public:
         void push_back(int data);
         void push_front(int data);
-        void insert_(int data);
+        void insert_(int index,int data);
+        void insert_after_node(Node *param_node);
         void pop_back();
         void pop_front();
         void pop();
-        Node* get(int index);
         void search(int index);
+        void empty_it(); // Delete Function
+        Node* get(int index);
         int size_();
-        ~LinkList();
 };
 
 Node* LinkList::get(int index){ // It will give you the pointer of that index
@@ -36,14 +37,14 @@ Node* LinkList::get(int index){ // It will give you the pointer of that index
     }
 
     Node *ptr_of_node = this->FirstElement;
-    while (index != 0){
+    while (index != 0){ // Iterating through each pointers
         ptr_of_node = ptr_of_node->nextElement;
         index--;
     };
     return ptr_of_node;
 }
 void LinkList::push_back(int data){
-    if(this->used_size == 0){
+    if(this->used_size == 0){ // Means This is the first data in this LinkList
         this->FirstElement->data = data;
         this->used_size++;
         return;
@@ -53,20 +54,41 @@ void LinkList::push_back(int data){
     this->LastElement = this->LastElement->nextElement;
     this->used_size++;
 }
+void LinkList::push_front(int data){
+    if(this->used_size == 0){ // Means This is the first data in this LinkList
+        this->FirstElement->data = data;
+        this->used_size++;
+        return;
+    }
+    Node *newNode = new Node; // First Creating a node then pointing it to head
+    newNode->data = data;
+    newNode->nextElement = this->FirstElement;
+    this->FirstElement = newNode;
+    this->used_size++;
+}
+void LinkList::insert_(int index,int data){
+    if(index == 0){
+        this->push_front(data);
+    }
+    else if(index == this->used_size){
+        this->push_back(data);
+    }
+}
 int LinkList::size_(){
     return this->used_size;
-}
-LinkList::~LinkList()
-{
 }
 
 int main(){
     LinkList testing_;
+    testing_.push_front(23);
     testing_.push_back(456);
     testing_.push_back(78);
     testing_.push_back(12);
     testing_.push_back(16);
     
-    std::cout<<testing_.get(3)->data<<std::endl;
+    std::cout<<testing_.get(0)->data<<std::endl;
+    std::cout<<testing_.get(1)->data<<std::endl;
+    std::cout<<testing_.get(2)->data<<std::endl;
+
     return 0;
 }
